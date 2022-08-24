bfedff568676b98c6230878886ff3a0c50dc6f07
Jess Archer <jess@jessarcher.com>
2022-08-24 10:27:33 +1000

    Add favicons

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,18 @@
 
         <title>{{ isset($title) ? $title . ' - ' : null }}Laravel Bootcamp</title>
 
+        <!-- Favicon -->
+        <link rel="apple-touch-icon" sizes="180x180" href="/img/favicon/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon/favicon-16x16.png">
+        <link rel="manifest" href="/img/favicon/site.webmanifest">
+        <link rel="mask-icon" href="/img/favicon/safari-pinned-tab.svg" color="#ff2d20">
+        <link rel="shortcut icon" href="/img/favicon/favicon.ico">
+        <meta name="msapplication-TileColor" content="#ff2d20">
+        <meta name="msapplication-config" content="/img/favicon/browserconfig.xml">
+        <meta name="theme-color" content="#ffffff">
+        <meta name="color-scheme" content="light">
+
         <!-- Fonts -->
         <link rel="stylesheet" href="https://use.typekit.net/ins2wgm.css">
 
