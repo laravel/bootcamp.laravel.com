13b55b8fad2b1defc68b1580cb0cf82154c95571
Jess Archer <jess@jessarcher.com>
2022-09-05 09:34:30 +1000

    Dark move improvements

--- resources/views/docs.blade.php
+++ resources/views/docs.blade.php
@@ -173,7 +173,7 @@
                                                     <x-cube delay="1000" class="hidden sm:block absolute bottom-0 right-0 mr-32 -mb-16" />
                                                     <img src="/img/screenshots/code-sample.png" class="xl:hidden mx-auto sm:ml-auto rotate-1 rounded-md w-5/6 sm:w-[450px]" />
                                                     <div class="mb-6 sm:mt-0 absolute bottom-0 xl:static">
-                                                        <div class="xl:-mr-24 p-3 sm:p-6 flex sm:inline-flex xl:flex space-x-2 rounded-lg border dark:border-dark-500 relative bg-white dark:bg-dark-600 shadow-xl z-10">
+                                                        <div class="xl:-mr-24 p-3 sm:p-6 flex sm:inline-flex xl:flex space-x-2 rounded-lg border dark:border-none relative bg-white dark:bg-dark-500 shadow-xl z-10">
                                                             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-600 dark:text-gray-500 -scale-x-100" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                                                                 <path stroke-linecap="round" stroke-linejoin="round" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
                                                             </svg>
