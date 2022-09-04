c1d5c206f15e0a231deb9f7202a9ea5a7852c4e6
Jess Archer <jess@jessarcher.com>
2022-09-05 09:27:20 +1000

    Responsive tweaks

--- resources/views/docs.blade.php
+++ resources/views/docs.blade.php
@@ -178,17 +178,17 @@
                                                                 <path stroke-linecap="round" stroke-linejoin="round" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
                                                             </svg>
                                                             <div class="flex-1">
-                                                                <div class="flex justify-between items-center">
+                                                                <div class="flex justify-between">
                                                                     <div>
                                                                         <span class="text-sm sm:text-base text-gray-800 dark:text-gray-200">Taylor Otwell</span>
                                                                         <br class="sm:hidden">
                                                                         <small class="sm:ml-2 text-xs sm:text-sm text-gray-600 dark:text-gray-500">a few seconds ago</small>
                                                                     </div>
-                                                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-gray-400 dark:text-gray-600" viewBox="0 0 20 20" fill="currentColor">
+                                                                    <svg xmlns="http://www.w3.org/2000/svg" class="mt-1 h-4 w-4 text-gray-400 dark:text-gray-600" viewBox="0 0 20 20" fill="currentColor">
                                                                         <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
                                                                     </svg>
                                                                 </div>
-                                                                <p class="mt-2 sm:mt-4 text-sm sm:text-lg text-gray-900 dark:text-gray-100">Join me in building an app with Laravel.</p>
+                                                                <p class="mt-2 sm:mt-4 mr-1 sm:mr-0 text-sm sm:text-lg text-gray-900 dark:text-gray-100">Join me in building an app with Laravel.</p>
                                                             </div>
                                                         </div>
                                                     </div>
