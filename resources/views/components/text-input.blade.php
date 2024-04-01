f5a083174db2061863aaab97b91006aeb4b47ae5
Shadonet <ingprs01@gmail.com>
2024-04-01 09:40:36 +0100

    add auth and logo

---
+++ resources/views/components/text-input.blade.php
@@ -0,0 +1,5 @@
+@props(['disabled' => false])
+
+<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border border-gray-200 bg-white text-gray-800 px-4 py-2 border-gray-300 dark:border-gray-700
+dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500
+dark:focus:ring-indigo-600 rounded-md shadow-sm']) !!}>
