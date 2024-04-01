fb28c66cb7422cb4885def6fe62f44c9db5cc06a
Shadonet <ingprs01@gmail.com>
2024-04-01 09:52:39 +0100

    fix margin issue

--- resources/views/docs.blade.php
+++ resources/views/docs.blade.php
@@ -10,7 +10,7 @@
                 class="min-h-screen hidden fixed top-0 bottom-0 left-0 z-20 h-full w-16 bg-gradient-to-b from-gray-100 to-white transition-all duration-300 overflow-hidden lg:sticky lg:w-80 lg:shrink-0 lg:flex lg:flex-col lg:justify-end lg:items-end 2xl:max-w-lg 2xl:w-full dark:from-dark-800 dark:to-dark-700"
             >
                 <div class="relative min-h-0 flex-1 flex flex-col xl:w-80">
-                    <a href="/" class="flex items-center py-8 px-4 lg:px-8 xl:px-16">
+                    <a href="/" class="flex items-center py-8 px-4 lg:px-8 xl:px-14">
                         <img
                             class="w-8 h-8 shrink-0 transition-all duration-300 lg:w-12 lg:h-12"
                             :class="{ 'w-12 h-12': navIsOpen }"
@@ -21,9 +21,9 @@
                         >
                         <span class="hidden lg:block ml-4 text-orange-500 text-3xl font-medium">Boostcamp</span>
                     </a>
-                    <div class="overflow-y-auto overflow-x-hidden px-4 lg:overflow-hidden lg:px-8 xl:px-16">
+                    <div class="overflow-y-auto overflow-x-hidden px-4 lg:overflow-hidden lg:px-8 xl:px-14">
                         <nav id="indexed-nav" class="hidden lg:block lg:mt-4">
-                            <div class="docs_sidebar">
+                            <div class="docs_sidebar ml-4">
                                 @include('documentation')
                             </div>
                         </nav>
